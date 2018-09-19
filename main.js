1ea37b220c5f42d3e5da008248e9881db4b0bcbf
Mary West <westmarye1219@gmail.com>
2018-09-18 19:47:24 -0500

    added code

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('I Loaded!');
+const toDoInputElm = document.getElementById('toDoInput');
+const notesInputElm = document.getElementById('notesInput');
+
+const submitToDoButtonElm = document.getElementById('submitToDoButton');
+
+const printToDom = (stringToPrint, whereToPrint) => {
+    document.getElementById(whereToPrint).innerHTML += stringToPrint;
+}
+
+const buildNewToDoCard = (toDo, notes) => {
+    let domString = `<div class="card" style="width: 18rem;">
+    <div class ="card-body">
+    <h5 class="card-title">${toDo}</h5>
+    <p class="card-text">${notes}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`;
+
+
+    printToDom(domString, 'toDoCards');
+}
+
+
+submitToDoButtonElm.addEventListener("click", (e) => {
+    e.preventDefault();
+
+buildNewToDoCard(toDoInputElm.value, notesInputElm.value);
+});
